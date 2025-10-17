c55c1dbac2b1a895bd5f6f455442d333014a8001
xxxkatyanxxx <xxxkatyanxxx@gmail.com>
2025-10-17 18:55:00 +0700

    Доработка логики 3

--- src/main/java/com/steup/kor/KorApplication.java
+++ src/main/java/com/steup/kor/KorApplication.java
@@ -1,6 +1,7 @@
 package com.steup.kor;
 
-public static void main(String[] args) {
-    System.out.println("Hello World");
-}
+public class KorApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World2");
+    }
 }
