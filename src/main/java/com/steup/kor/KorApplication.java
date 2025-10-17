eaa1ac93f6890e45a14e785a25132badf11662f0
xxxkatyanxxx <xxxkatyanxxx@gmail.com>
2025-10-17 18:59:09 +0700

    Доработка логики 4

--- src/main/java/com/steup/kor/KorApplication.java
+++ src/main/java/com/steup/kor/KorApplication.java
@@ -3,5 +3,6 @@ package com.steup.kor;
 public class KorApplication {
     public static void main(String[] args) {
         System.out.println("Hello World2");
+        System.out.println("Hello World2");
     }
 }
