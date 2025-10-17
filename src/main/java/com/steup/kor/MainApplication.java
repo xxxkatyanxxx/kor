b57b804173973c3a5ca73701e579eed861212c73
xxxkatyanxxx <xxxkatyanxxx@gmail.com>
2025-10-17 13:54:35 +0700

    Доработка логики 2

--- src/main/java/com/steup/kor/MainApplication.java
+++ src/main/java/com/steup/kor/MainApplication.java
@@ -3,6 +3,9 @@ package com.steup.kor;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("111");
+        System.out.println("101");
+        System.out.println("222");
+        System.out.println("333");
+
     }
 }
